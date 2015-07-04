45d45b04e1a0a0bd69b1b5edaa79adf1e11deb1f
koogawa <koogawa.app@gmail.com>
2015-07-04 23:59:14 +0900

    Init

--- WatchConnectivitySample WatchKit Extension/InterfaceController.m
+++ WatchConnectivitySample WatchKit Extension/InterfaceController.m
@@ -7,32 +7,83 @@
 //
 
 #import "InterfaceController.h"
+#import <WatchConnectivity/WatchConnectivity.h>
 
-
-@interface InterfaceController()
-
+@interface InterfaceController() <WCSessionDelegate>
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *resultLabel;
 @end
-
 
 @implementation InterfaceController
 
-- (void)awakeWithContext:(id)context {
+- (void)awakeWithContext:(id)context
+{
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    if ([WCSession isSupported]) {
+        WCSession *session = [WCSession defaultSession];
+        session.delegate = self;
+        [session activateSession];
+    }
 }
 
-- (void)willActivate {
+- (void)willActivate
+{
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+
+    // Application Context
+    NSDictionary *applicationDict = @{@"hoge" : @"huga"};
+    NSError *error = nil;
+    [[WCSession defaultSession] updateApplicationContext:applicationDict error:&error];
+    NSLog(@"error = %@", error);
 }
 
-- (void)didDeactivate {
+- (void)didDeactivate
+{
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
 }
 
+#pragma mark - WCSessionDelegate
+
+- (void)sessionWatchStateDidChange:(WCSession *)session
+{
+    NSLog(@"%s: session = %@", __func__, session);
+}
+
+// Application Context
+- (void)session:(nonnull WCSession *)session didReceiveApplicationContext:(nonnull NSDictionary<NSString *,id> *)applicationContext
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.resultLabel setText:[NSString stringWithFormat:@"%s: %@", __func__, applicationContext]];
+    });
+}
+
+// User Info Transfer
+- (void)session:(nonnull WCSession *)session didReceiveUserInfo:(nonnull NSDictionary<NSString *,id> *)userInfo
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.resultLabel setText:[NSString stringWithFormat:@"%s: %@", __func__, userInfo]];
+    });
+}
+
+// File Transfer
+- (void)session:(nonnull WCSession *)session didReceiveFile:(nonnull WCSessionFile *)file
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.resultLabel setText:[NSString stringWithFormat:@"%s: %@", __func__, file]];
+    });
+}
+
+// Interactive Message
+- (void)session:(nonnull WCSession *)session didReceiveMessage:(nonnull NSDictionary<NSString *,id> *)message replyHandler:(nonnull void (^)(NSDictionary<NSString *,id> * __nonnull))replyHandler
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.resultLabel setText:[NSString stringWithFormat:@"%s: %@", __func__, message]];
+    });
+
+    replyHandler(@{@"reply" : @"OK"});
+}
+
 @end
-
-
-
--- WatchConnectivitySample/ViewController.m
+++ WatchConnectivitySample/ViewController.m
@@ -7,19 +7,60 @@
 //
 
 #import "ViewController.h"
+#import <WatchConnectivity/WatchConnectivity.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *resultTextView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+
+    // Application Context
+    NSDictionary *applicationDict = @{@"hoge" : @"huga"};
+    NSError *error = nil;
+    [[WCSession defaultSession] updateApplicationContext:applicationDict error:&error];
+    NSLog(@"error = %@", error);
+
+    // User Info Transfer
+    [[WCSession defaultSession] transferUserInfo:applicationDict];
+
+    // File Transfer
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"marimo" ofType:@"png"]];
+    WCSessionFileTransfer *fileTransfer = [[WCSession defaultSession] transferFile:url metadata:applicationDict];
+    NSLog(@"fileTransfer = %@", fileTransfer);
+
+    // Interactive Message
+    if ([[WCSession defaultSession] isReachable])
+    {
+        [[WCSession defaultSession] sendMessage:applicationDict
+                                   replyHandler:^(NSDictionary *replyHandler) {
+                                       NSLog(@"replyHandler = %@", replyHandler);
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           self.resultTextView.text = [NSString stringWithFormat:@"%@", replyHandler];
+                                       });
+                                   }
+                                   errorHandler:^(NSError *error) {
+                                       NSLog(@"error = %@", error);
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           self.resultTextView.text = [NSString stringWithFormat:@"%@", error];
+                                       });
+                                   }
+         ];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
